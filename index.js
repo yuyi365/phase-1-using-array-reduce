01b7a80c4552702b67fc6ce073551ed135ee4c35
Yuyi <yuyi365@gmail.com>
2021-10-14 17:41:58 -0400

    commit tree 6c9028dc3aff3f264a9c60f86d94eba24069515e

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+//create a variable totalBatteries that holds the sum of all of the battery amounts in the batteryBatches array
+
+const reducerTool = (accumulator, item) => {
+    return accumulator += item; // sum of total per item in the array
+}
+// returns each acculated amount by item
+
+let totalBatteries = batteryBatches.reduce(reducerTool, 0);
+totalBatteries;
+
+// const totalBatteries = batteryBatches.reduce(function(total, batches){
+//     return batches + total
+//     }
+//   )
